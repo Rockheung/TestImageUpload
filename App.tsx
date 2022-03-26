edcb03bb2b1875496ba2b6b33e4e21aa712f1c09
Rockheung <rockheung@gmail.com>
2022-03-26 21:32:21 +0900

    complete upload

--- App.tsx
+++ App.tsx
@@ -8,107 +8,161 @@
  * @format
  */
 
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
+  Dimensions,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
+  Text,
+  ScrollView,
+  TouchableOpacity,
   View,
+  Platform,
+  Image,
 } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Header = forwardRef((props, ref) => {
+  const handleCamera = React.useCallback(() => {
+    ImagePicker.openCamera({
+      multiple: true,
+      forceJPG: true,
+    })
+      .then(images => {
+        const _images = images.map(image => {
+          return Platform.OS === 'ios'
+            ? {...image, path: image.sourceURL}
+            : image;
+        });
+        props.setImages(_images);
+      })
+      .catch(console.warn);
+  }, [props]);
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleLibrary = React.useCallback(() => {
+    ImagePicker.openPicker({
+      multiple: true,
+      forceJPG: true,
+    })
+      .then(images => {
+        const _images = images.map(image => {
+          return Platform.OS === 'ios'
+            ? {...image, path: image.sourceURL}
+            : image;
+        });
+        props.setImages(_images);
+      })
+      .catch(console.warn);
+  }, [props]);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View ref={ref} style={styles.header}>
+      <TouchableOpacity style={styles.button} onPress={handleCamera}>
+        <Text>{'촬영'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleLibrary}>
+        <Text>{'앨범'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={props.sendImages}>
+        <Text>{'전송'}</Text>
+      </TouchableOpacity>
     </View>
   );
-};
+});
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [images, setImages] = React.useState<any[]>([null]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const handleImages = React.useCallback(newImages => {
+    setImages(prevImages => {
+      return [null, ...prevImages, ...newImages];
+    });
+  }, []);
+
+  const handleSubmit = React.useCallback(async () => {
+    const _form = new FormData();
+    images.map((image, idx) => {
+      _form.append('image_' + idx, {
+        type: image.mime,
+        name: 'image_' + idx,
+        uri: image.path,
+      });
+    });
+    const res = await fetch('http://localhost:8080', {
+      method: 'POST',
+      body: _form,
+    });
+    console.log(res);
+  }, [images]);
+
+  React.useEffect(() => {
+    console.log(
+      '🚀 ~ file: App.tsx ~ line 73 ~ App ~ images',
+      JSON.stringify(images, null, 2),
+    );
+  }, [images]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.wrapper}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        style={styles.wrapper}
+        stickyHeaderHiddenOnScroll={false}
+        StickyHeaderComponent={forwardRef((props, ref) => (
+          <Header
+            ref={ref}
+            setImages={handleImages}
+            sendImages={handleSubmit}
+          />
+        ))}
+        stickyHeaderIndices={[0]}
+        contentContainerStyle={styles.contentContainer}>
+        {images.slice(1).length ? (
+          images.slice(1).map((image, idx) => {
+            const width = Math.floor(Dimensions.get('screen').width / 2);
+            const height = (width * image.height) / image.width;
+            return (
+              <Image
+                style={{width, height}}
+                key={image.path + '/' + idx}
+                source={{uri: image.path}}
+                resizeMode={'contain'}
+              />
+            );
+          })
+        ) : (
+          <Text>{'No Image Found'}</Text>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  wrapper: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    flexDirection: 'row',
+    backgroundColor: 'lightyellow',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    zIndex: 99,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    flexBasis: '20%',
+    backgroundColor: 'lightblue',
+    padding: 24,
+    margin: 12,
+    borderRadius: 30,
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  contentContainer: {
+    // flex: 1,
+    backgroundColor: 'pink',
   },
 });
 
---
+++ server/app.js
@@ -0,0 +1,34 @@
+const {randomFillSync} = require('crypto');
+const fs = require('fs');
+const http = require('http');
+const os = require('os');
+const path = require('path');
+
+const busboy = require('busboy');
+
+const random = (() => {
+  const buf = Buffer.alloc(16);
+  return () => randomFillSync(buf).toString('hex');
+})();
+
+http
+  .createServer((req, res) => {
+    if (req.method === 'POST') {
+      const bb = busboy({headers: req.headers});
+      bb.on('file', (name, file, info) => {
+        const saveTo = path.join(__dirname, `busboy-upload-${random()}`);
+        file.pipe(fs.createWriteStream(saveTo));
+      });
+      bb.on('close', () => {
+        res.writeHead(200, {Connection: 'close'});
+        res.end(`That's all folks!`);
+      });
+      req.pipe(bb);
+      return;
+    }
+    res.writeHead(404);
+    res.end();
+  })
+  .listen(8080, () => {
+    console.log('Listening for requests');
+  });
